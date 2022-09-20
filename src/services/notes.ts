ccbf713bbaa1e24a27b31bc34312762243756a1b
Pablo Rodriguez <pablorodriguez833@gmail.com>
2022-09-20 11:37:03 -0300

    feat: add token in request get notes

--- src/services/notes.ts
+++ src/services/notes.ts
@@ -4,6 +4,18 @@ import { noteType } from "../models/note.type";
 // const API_BASE = "https://stormy-brook-97359.herokuapp.com/api/notes";
 const API_BASE = "http://localhost:3001/api/notes";
 
+const getConfig = () => {
+  const storedAuth = window.sessionStorage.getItem("auth");
+  const parseStoredAuth = storedAuth && JSON.parse(storedAuth);
+
+  return {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${parseStoredAuth.token}`,
+    },
+  };
+};
+
 export const postNote = (note: noteType) => {
   return axios
     .post(API_BASE, note)
@@ -27,12 +39,9 @@ export const putNote = (note: noteType) => {
 };
 
 export const getNotes = () => {
-  const storedAuth = window.sessionStorage.getItem("auth");
-  const parseStoredAuth = storedAuth && JSON.parse(storedAuth);
-  console.log("turboCL -> file: notes.ts -> line 32 -> getNotes -> parseStoredAuth", parseStoredAuth)
-
+  const config = getConfig();
   return axios
-    .get(API_BASE)
+    .get(API_BASE, config)
     .then((response) => {
       return response.data;
     })
